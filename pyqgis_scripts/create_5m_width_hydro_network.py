15ba4ccfa8d1619e396e6aec6531ca854e7121c1
Manière Louis <louismaniere@orange.fr>
2024-01-08 17:34:34 +0100

    5m width hydro network fix with outlet

--- pyqgis_scripts/create_5m_width_hydro_network.py
+++ pyqgis_scripts/create_5m_width_hydro_network.py
@@ -194,7 +194,25 @@ def create_5m_width_hydro_network(surface_hydrographique_gpkg,
     IdentifyNetworkNodes.dataProvider().createSpatialIndex()
     print('IdentifyNetworkNodes index created')
 
-    IdentifyNetworkNodes_copy = IdentifyNetworkNodes.clone()
+    IdentifyNetworkNodes.selectAll()
+    IdentifyNetworkNodes_copy = processing.run("native:saveselectedfeatures", {'INPUT': IdentifyNetworkNodes, 
+                                                                            'OUTPUT': 'TEMPORARY_OUTPUT'})['OUTPUT']
+    IdentifyNetworkNodes.removeSelection()
+
+    # save output
+    saving_gpkg(IdentifyNetworkNodes_copy, "IdentifyNetworkNodes_copy", wd + "work/work_reference_5m_rmc.gpkg", save_selected=False)
+
+    # extract outlet by exutoire, we need final outlet to fix network connectivity
+    outlet = processing.run('native:extractbylocation',
+        {
+            'INPUT' : IdentifyNetworkNodes, 
+            'INTERSECT' : exutoire_layer,
+            'PREDICATE' : [0],
+            'OUTPUT' : 'TEMPORARY_OUTPUT'
+        })['OUTPUT']
+    
+    # save output
+    saving_gpkg(outlet, "outlet", wd + "work/work_reference_5m_rmc.gpkg", save_selected=False)
     
     # get only stream with % of their length inside the water surface => percent_stream_in_surface
     pc_length_in_surface_field = 'length_in_surface'
@@ -235,116 +253,28 @@ def create_5m_width_hydro_network(surface_hydrographique_gpkg,
     IdentifyNetworkNodes.commitChanges()
 
     # save output
-    saving_gpkg(IdentifyNetworkNodes, "IdentifyNetworkNodes", wd + "work/work_reference_5m_rhone_leman.gpkg", save_selected=False)
+    saving_gpkg(IdentifyNetworkNodes, "IdentifyNetworkNodes", wd + "work/work_reference_5m_rmc.gpkg", save_selected=False)
 
-    # extract outlet by exutoire, we need final outlet to fix network connectivity
-    outlet = processing.run('native:extractbylocation',
-        {
-            'INPUT' : IdentifyNetworkNodes_copy, 
-            'INTERSECT' : exutoire_layer,
-            'PREDICATE' : [0],
-            'OUTPUT' : 'TEMPORARY_OUTPUT'
-        })['OUTPUT']
-    
     # merge outlet and IdentifyNetworkNodes
-    merge_outlet_IdentifyNetworkNodes = processing.run('native:mergevectorlayers',
-        {
-            'LAYERS' : [outlet, IdentifyNetworkNodes],
-            'CRS' : None,
-            'OUTPUT' : 'TEMPORARY_OUTPUT'
-        })['OUTPUT']
+    processing.run('etl_load:appendfeaturestolayer',
+        { 'ACTION_ON_DUPLICATE' : 1, 
+        'SOURCE_FIELD' : 'fid', 
+        'SOURCE_LAYER' : outlet,
+        'TARGET_FIELD' : 'fid', 
+        'TARGET_LAYER' : IdentifyNetworkNodes})
     
-    # remove working fields
-    with edit(merge_outlet_IdentifyNetworkNodes):
-        # Find the field indexes of the fields you want to remove
-        layer_index = merge_outlet_IdentifyNetworkNodes.fields().indexFromName("layer")
-        path_index = merge_outlet_IdentifyNetworkNodes.fields().indexFromName("path")
-        # Delete the attributes (fields) using the field indexes
-        merge_outlet_IdentifyNetworkNodes.dataProvider().deleteAttributes([layer_index, path_index])
-        # Update the fields to apply the changes
-        merge_outlet_IdentifyNetworkNodes.updateFields()
-
-    # remove duplicate from merging
-    merge_no_duplicate = processing.run('native:deleteduplicategeometries',
-                   {
-                       'INPUT' : merge_outlet_IdentifyNetworkNodes,
-                       'OUTPUT': 'TEMPORARY_OUTPUT'
-                   })['OUTPUT']
-
     # fix network connectivity
     fixed_network = processing.run('fct:fixnetworkconnectivity', 
         {
             'INPUT' : IdentifyNetworkNodes_copy, 
-            'SUBSET' : merge_no_duplicate, 
+            'SUBSET' : IdentifyNetworkNodes, 
             'FROM_NODE_FIELD' : 'NODEA', 
             'TO_NODE_FIELD' : 'NODEB',
             'OUTPUT' : 'TEMPORARY_OUTPUT', 
         })['OUTPUT']
     
-    # remove working fields
-    with edit(fixed_network):
-        node_a_index = fixed_network.fields().indexFromName("NODEA")
-        node_b_index = fixed_network.fields().indexFromName("NODEB")
-        # Delete the attributes (fields) using the field indexes
-        fixed_network.dataProvider().deleteAttributes([node_a_index, node_b_index])
-        # Update the fields to apply the changes
-        fixed_network.updateFields()
-    
-    # Identify Network Nodes
-    print('New IdentifyNetworkNodes processing, this could take some time')
-    IdentifyNetworkNodes_2 = processing.run('fct:identifynetworknodes', 
-        {
-            'INPUT': fixed_network,
-            'QUANTIZATION': 100000000,
-            'NODES': 'TEMPORARY_OUTPUT',
-            'OUTPUT': 'TEMPORARY_OUTPUT'
-        })['OUTPUT']
-    
-    
-    # Aggregate reaches to intersection
-    fields = IdentifyNetworkNodes_2.fields()
-    field_names = [field.name() for field in fields if field.name() not in ['NODEA', 'NODEB']] # get all fields but NODEA and NODEB in a list to copy it
-    print('Aggregate reaches to intersection')
-    # need this trick with QgsProcessingFeatureSourceDefinition(IdentifyNetworkNodes_2.source()) 
-    # to avoid unsolved error AttributeError: 'NoneType' object has no attribute 'getFeature' with AggregateSegment
-    QgsProject.instance().addMapLayer(IdentifyNetworkNodes_2)
-
-    AggregateSegment = processing.run('fct:aggregatestreamsegments',
-                                     {
-                                        'INPUT': QgsProcessingFeatureSourceDefinition(IdentifyNetworkNodes_2.source()),
-                                        'CATEGORY_FIELD' : '',
-                                        'COPY_FIELDS' : field_names,
-                                        'FROM_NODE_FIELD' : 'NODEA',
-                                        'TO_NODE_FIELD' : 'NODEB',
-                                        'OUTPUT' : 'TEMPORARY_OUTPUT'
-                                     })['OUTPUT']
-
-    QgsProject.instance().removeMapLayer(IdentifyNetworkNodes_2)
-
-    # remove working fields
-    with edit(AggregateSegment):
-        # Find the field indexes of the fields you want to remove
-        gid_index = AggregateSegment.fields().indexFromName("GID")
-        length_index = AggregateSegment.fields().indexFromName("LENGTH")
-        category_index = AggregateSegment.fields().indexFromName("CATEGORY")
-        node_a_index = AggregateSegment.fields().indexFromName("NODEA")
-        node_b_index = AggregateSegment.fields().indexFromName("NODEB")
-        # Delete the attributes (fields) using the field indexes
-        AggregateSegment.dataProvider().deleteAttributes([gid_index, length_index, category_index, node_a_index, node_b_index])
-        # Update the fields to apply the changes
-        AggregateSegment.updateFields()
-    
-    # Identify Network Nodes
-    print('New IdentifyNetworkNodes processing, this could take some time')
-    IdentifyNetworkNodes_3 = processing.run('fct:identifynetworknodes', 
-        {
-            'INPUT': AggregateSegment,
-            'QUANTIZATION': 100000000,
-            'NODES': 'TEMPORARY_OUTPUT',
-            'OUTPUT': 'TEMPORARY_OUTPUT'
-        })['OUTPUT']
-
-    QgsProject.instance().addMapLayer(IdentifyNetworkNodes_3)
+    # save output
+    saving_gpkg(fixed_network, "fixed_network", wd + "work/work_reference_5m_rmc.gpkg", save_selected=False)
 
     # Measure network from outlet
     print('Measure network from outlet')
@@ -352,11 +282,12 @@ def create_5m_width_hydro_network(surface_hydrographique_gpkg,
         {
             'FROM_NODE_FIELD' : 'NODEA',
             'TO_NODE_FIELD' : 'NODEB',
-            'INPUT' : QgsProcessingFeatureSourceDefinition(IdentifyNetworkNodes_3.source()), 
+            'INPUT' : fixed_network, 
             'OUTPUT' : 'TEMPORARY_OUTPUT', 
         })['OUTPUT']
-    
-    QgsProject.instance().removeMapLayer(IdentifyNetworkNodes_3)
+
+    # save output
+    saving_gpkg(networkMeasureFromOutlet, "networkMeasureFromOutlet", wd + "work/work_reference_5m_rmc.gpkg", save_selected=False)
     
     # Hack order
     print('Compute Hack order')
@@ -369,6 +300,9 @@ def create_5m_width_hydro_network(surface_hydrographique_gpkg,
             'MEASURE_FIELD' : 'MEASURE', 
             'OUTPUT' : 'TEMPORARY_OUTPUT', 
         })['OUTPUT']
+    
+    # save output
+    saving_gpkg(networkHack, "networkHack", wd + "work/work_reference_5m_rmc.gpkg", save_selected=False)
     
     # Strahler order
     print('Compute Strahler order')
@@ -412,25 +346,86 @@ def create_5m_width_hydro_network(surface_hydrographique_gpkg,
     networkStrahler.startEditing()
     networkStrahler.deleteSelectedFeatures()
     networkStrahler.commitChanges()
-    
-    # remove working fields
-    with edit(networkStrahler):
-        # Find the field indexes of the fields you want to remove
-        fid_index = networkStrahler.fields().indexFromName("fid") # remove fid before save to regenerate fid when gpkg created (can cause unique fid error)
-        node_a_index = networkStrahler.fields().indexFromName("NODEA")
-        node_b_index = networkStrahler.fields().indexFromName("NODEB")
-        measure_index = networkStrahler.fields().indexFromName("MEASURE")
-        length_index = networkStrahler.fields().indexFromName("LENGTH")
-        axis_index = networkStrahler.fields().indexFromName("AXIS")
-        laxis_index = networkStrahler.fields().indexFromName("LAXIS")
-        # Delete the attributes (fields) using the field indexes
-        networkStrahler.dataProvider().deleteAttributes([fid_index, node_a_index, node_b_index, measure_index,
-                                                         length_index, axis_index, laxis_index])
-        # Update the fields to apply the changes
-        networkStrahler.updateFields()
 
     # save output
-    saving_gpkg(networkStrahler, reference_hydrographique_5m_layername, reference_hydrographique_5m_gpkg_path, save_selected=False)
+    saving_gpkg(networkStrahler, "networkStrahler", wd + "work/work_reference_5m_rmc.gpkg", save_selected=False)
+
+    # reaggregate stream after sliver stream  removed
+    print('Aggregate reaches to intersection')
+    AggregateSegment = processing.run('fct:aggregatestreamsegments',
+                                        {
+                                        'INPUT': networkStrahler,
+                                        'CATEGORY_FIELD' : '',
+                                        'COPY_FIELDS' : 'fid',
+                                        'FROM_NODE_FIELD' : 'NODEA',
+                                        'TO_NODE_FIELD' : 'NODEB',
+                                        'OUTPUT' : 'TEMPORARY_OUTPUT'
+                                        })['OUTPUT']
+    
+    # save output
+    saving_gpkg(AggregateSegment, "AggregateSegment", wd + "work/work_reference_5m_rmc.gpkg", save_selected=False)
+    
+    join_aggregate = processing.run('native:joinattributestable',
+                                { 
+                                    'DISCARD_NONMATCHING' : False, 
+                                    'FIELD' : 'fid', 
+                                    'FIELDS_TO_COPY' : [], 
+                                    'FIELD_2' : 'fid', 
+                                    'INPUT' : AggregateSegment, 
+                                    'INPUT_2' : networkStrahler, 
+                                    'METHOD' : 1, 
+                                    'OUTPUT' : 'TEMPORARY_OUTPUT', 
+                                    'PREFIX' : 'join_' 
+                                    })['OUTPUT']
+    
+    # save output
+    saving_gpkg(join_aggregate, "join_aggregate", wd + "work/work_reference_5m_rmc.gpkg", save_selected=False)
+    
+    # rename fields
+    with edit(join_aggregate):
+        for field in join_aggregate.fields():
+            # Check if the field name starts with "join_"
+            if field.name().startswith('join_'):
+                new_field_name = field.name()[5:]  # Remove the first 5 characters ("join_")
+                idx = join_aggregate.fields().indexFromName(field.name())
+                join_aggregate.renameAttribute(idx, new_field_name)
+
+    fields_to_remove = ["fid", "join_fid", 
+                        "NODEA", "join_NODEA", 
+                        "NODEB", "join_NODEB",
+                        "MEASURE", "length_in_surface",
+                        "LENGTH", "join_LENGTH",
+                        "GID",
+                        "CATEGORY",
+                    "AXIS", "LAXIS"]
+
+    # remove fields
+    field_indexes = {field_name: join_aggregate.fields().indexFromName(field_name) for field_name in fields_to_remove}
+
+    with edit(join_aggregate):
+        # Delete the attributes (fields) using the field indexes
+        join_aggregate.dataProvider().deleteAttributes(list(field_indexes.values()))
+        
+        # Update the fields to apply the changes
+        join_aggregate.updateFields()
+
+    # add a new length field
+    with edit(join_aggregate):
+        new_field = QgsField("length", QVariant.Double)
+        join_aggregate.dataProvider().addAttributes([new_field])
+        join_aggregate.updateFields()
+
+        length_index = join_aggregate.fields().indexFromName("length")
+
+        for feature in join_aggregate.getFeatures():
+            length = feature.geometry().length()
+            
+            join_aggregate.dataProvider().changeAttributeValues({feature.id(): {length_index: length}})
+
+    join_aggregate.commitChanges()
+
+    # save output
+    saving_gpkg(join_aggregate, reference_hydrographique_5m_layername, reference_hydrographique_5m_gpkg_path, save_selected=False)
 
     print('End : hydrological reference network 5m from hydrographic surface created')
 
@@ -445,6 +440,6 @@ create_5m_width_hydro_network(surface_hydrographique_gpkg = 'surface_hydrographi
                               exutoire_gpkg = 'exutoire.gpkg', 
                               exutoire_buffer_layername = 'exutoire_buffer50',
                               zone_gpkg = 'zone.gpkg',
-                              zone_layername = 'rhone_leman_annaz',
+                              zone_layername = 'rmc',
                               small_segment_filter = 500,
                               percent_stream_in_surface = 30)
